b16845f71f7098fe7d2942af8a5aa5bcb12218d8
webdizzy777 <carissa.mcmillian@gmail.com>
2025-09-14 16:21:40 -0500

    footer placement

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ function App() {
   
 
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <Header></Header>
+      <main className="flex-fill">
       <Home></Home>
+      </main>s
       <Footer></Footer>
-    </>
+    </div>
   )
 }
 
