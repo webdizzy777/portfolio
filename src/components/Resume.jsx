a5fa3615623ad3cba8238cf8f457ae8c8ceba271
webdizzy777 <carissa.mcmillian@gmail.com>
2025-09-15 17:55:49 -0500

    added resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,12 +1,20 @@
+import resume from "../assets/McMillian_Carissa_Resume_Linkedin.pdf"
+
 function Resume() {
   return (
     <section id="resume" className="container my-5">
       <h2 className="mb-4">Resume</h2>
       <p>
-        <a href="/resume.pdf" download>
+        <a href={resume} download>
           Download my resume
         </a>
       </p>
+      <iframe
+        src={resume}
+        width="100%"
+        height="600px"
+        title="Resume Preview"
+      />
     </section>
   );
 }
